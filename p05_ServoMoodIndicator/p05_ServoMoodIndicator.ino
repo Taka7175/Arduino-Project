478e28835580569fa0ebec936efc98bfc3eb8971
gdm-9961 <159160385+gdm-9961@users.noreply.github.com>
2024-04-17 18:19:19 -0400

    Update ServoMoodIndicator
    wrote main program loop including all components, just placeholder values for now

--- p05_ServoMoodIndicator/p05_ServoMoodIndicator.ino
+++ p05_ServoMoodIndicator/p05_ServoMoodIndicator.ino
@@ -1,76 +1,71 @@
 /*
-  Arduino Starter Kit example
-  Project 5 - Servo Mood Indicator
-
-  This sketch is written to accompany Project 5 in the Arduino Starter Kit
-
-  Parts required:
-  - servo motor
-  - 10 kilohm potentiometer
-  - two 100 uF electrolytic capacitors
-
+  This code is adapted from starter kit Project 5,
   created 13 Sep 2012
   by Scott Fitzgerald
 
   https://store.arduino.cc/genuino-starter-kit
-
-  This example code is part of the public domain.
 */
 
 // include the Servo library
 #include <Servo.h>
+// spinner
+Servo spinAround;
+int servoSpinPin = 5;
+int sAngle = 180;
 
-Servo myServo;  // create a servo object
+// kicker
+Servo kicker;
+int servoKickPin = 0;
+int kAngle = 160;
 
-int const potPin = A0;  // analog pin used to connect the potentiometer
-int potVal;             // variable to read the value from the analog pin
-int angle; 
-int fsrAnalogPin = A1; // FSR is connected to analog 0
-//int LEDpin = 9;      // connect Red LED to pin 11 (PWM pin)
-int fsrReading;      // the analog reading from the FSR resistor divider
-//int LEDbrightness;             // variable to hold the angle for the servo motor
+// kickee
+int const pressPin = A0;
+int pressVal = 1000;
+int pressThresh = 400;
 
 void setup() {
-  myServo.attach(6); 
-  //Serial.begin(9600);
-    // attaches the servo on pin 9 to the servo object
-  //Serial.begin(9600);  // open a serial connection to your computer
+  spinAround.attach(servoSpinPin); 
+  kicker.attach(servoKickPin);
+  
+  // debug
+  // Serial.begin(9600);
+  // Serial.println("Hello world");
 }
 
 void loop() {
-  potVal = analogRead(potPin); 
-   // read the value of the potentiometer
-  // print out the value to the Serial Monitor
-  //Serial.print("potVal: ");
-  //Serial.print(potVal);
+  while (pressVal > pressThresh){
+    pressVal = analogRead(pressPin);
+  }
+  /* debug
+  Serial.print("pressVal: ");
+  Serial.println(pressVal);
+  delay(200);*/
+ 
+ spinAround.write(sAngle);
+ delay(1000);
+ kicker.write(kAngle);
+ 
+ while (pressVal < pressThresh){
+  pressVal = analogRead(pressPin);
+ }
 
-  // scale the numbers from the pot
-  //angle = map(potVal, 0, 1023, 0, 179);
-  angle = 100;
+ kicker.write(kAngle);
+ delay(1000);
+ spinAround.write(sAngle);
 
-  fsrReading = analogRead(fsrAnalogPin);
-  myServo.write(angle);
-  //Serial.println(fsrReading);
-  printf(fsrReading);
-  delay(1000);
-  
+ delay(10000);
 
-  // print out the angle for the servo motor
-  //Serial.print(", angle: ");
-  //Serial.println(angle);
-// int loops = 0;
-//   // set the servo position
-//   while(loops <= 5)
-//   {
-//     myServo.write(angle);
-//     delay(1000);
-//     myServo.write(0);
-//     delay(1000);
-//     loops++;
-//   }
-  //myServo.write(0);
-
-// wait for the servo to get there
-exit(0);  
-
+ /* debug
+ int loops = 0;
+   // set the servo position
+   while(loops <= 5)
+   {
+     spinAround.write(angle);
+     delay(3000);
+     spinAround.write(0);
+     delay(3000);
+     loops++;
+   }
+  //spinAround.write(0); 
+  */
 }
